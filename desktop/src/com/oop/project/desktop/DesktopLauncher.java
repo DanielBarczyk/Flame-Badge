4939333f2aacc35b553415f41185853b5f9959ea
DanielBarczyk <d.barczyk99@gmail.com>
2020-03-27 21:13:46 +0100

    initial commit

---
+++ desktop/src/com/oop/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oop.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oop.project.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
